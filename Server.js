8f54635fa9529094c03a6f9b5c3c3f60b14c3049
bhagwat-jadhav <bhagwatjadhav649@gmail.com>
2025-07-21 19:53:53 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,6 +3,6 @@ const path=require ('path');
 const app= express ();
 
 app.get('/',(req,res)=>{
-  res.sendFile(path.join(__dirname,'myapp--savr');
+  res.sendFile(path.join(__dirname,'myapp--savr','src','App.jsx');
 });
 app.listen(3000);
